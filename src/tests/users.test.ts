ba0b38230c3e06ebdf29cf5ee76a08c450d3dba9
César Rafael <cesarrdev@gmail.com>
2023-01-10 17:15:50 -0300

    feat: add test post users - Should not to be able to create a user that already exists

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import app from "../app";
 import AppDataSource from "../data-source";
+import { User } from "../entities/user.entity";
 import {
   createUserValid,
   mockedAdmin,
@@ -12,6 +13,7 @@ import {
 describe("POST/users", () => {
   let connection: DataSource;
   const baseUrl = "/users";
+  const userRepo = AppDataSource.getRepository(User);
 
   beforeAll(async () => {
     await AppDataSource.initialize()
@@ -21,6 +23,11 @@ describe("POST/users", () => {
       .catch((err) => {
         console.error("Error during Data Source initialization", err);
       });
+  });
+
+  beforeEach(async () => {
+    const users = await userRepo.find();
+    await userRepo.remove(users);
   });
 
   afterAll(async () => {
@@ -33,6 +40,7 @@ describe("POST/users", () => {
     expect(response.status).toBe(201);
     expect(response.body).toEqual(
       expect.objectContaining({
+        id: "uuid",
         isAdmin: false,
         username: "testename",
       })
@@ -40,34 +48,84 @@ describe("POST/users", () => {
     expect(response.body).not.toHaveProperty("password");
   });
 
-  test("POST /users - should not to be able to create a user that already exists", async () => {
+  test("POST /users - Should not to be able to create a user that already exists", async () => {
+    const userReq = userRepo.create(createUserValid);
+    await userRepo.save(userReq);
+
     const response = await request(app).post("/users").send(createUserValid);
 
+    const [user, amount] = await userRepo.findAndCountBy({
+      id: response.body.id,
+    });
+    expect(amount).toBe(1);
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(409);
   });
 
+  test("POST /users - Should not be possible to create a user without administrator permissions", async () => {
+    const userLoginAdmin = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+
+    const response = await request(app)
+      .post("/users")
+      .set("Authorization", `Bearer ${userLoginAdmin.body.token}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: "uuid",
+        isAdmin: false,
+        username: "testename",
+      })
+    );
+  });
+
   test("GET /users - Must be able to list users", async () => {
     await request(app).post("/users").send(mockedAdmin);
-    const admLoginResp = await request(app)
+    const userLoginAdmin = await request(app)
       .post("/login")
       .send(mockedAdminLogin);
     const response = await request(app)
       .get("/users")
-      .set("Authorization", `Bearer ${admLoginResp.body.token}`);
+      .set("Authorization", `Bearer ${userLoginAdmin.body.token}`);
 
-    expect(response.body).toHaveLength(2);
+    expect(response.body).toHaveProperty("map");
     expect(response.body[0]).not.toHaveProperty("password");
   });
 
-  test("GET /users - should not be able to list users without authentication", async () => {
+  test("GET /users - It should be possible to list a especific user", async () => {
+    const userLoginAdmin = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+
+    const response = await request(app)
+      .get("/users")
+      .set("Authorization", `Bearer ${userLoginAdmin.body.token}`);
+
+    const userReq = userRepo.create(createUserValid);
+    await userRepo.save(userReq);
+
+    const userExists = await userRepo.find({
+      where: {
+        id: response.body.id,
+      },
+    });
+
+    expect(userExists).toHaveLength(1);
+    expect(response.body).toHaveProperty("map");
+    expect(response.status).toBe(200);
+  });
+
+  test("GET /users - Should not be able to list users without authentication", async () => {
     const response = await request(app).get("/users");
 
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(401);
   });
 
-  test("GET /users - should not be able to list users not being admin", async () => {
+  test("GET /users - Should not be able to list users not being admin", async () => {
     const userLoginResp = await request(app)
       .post("/login")
       .send(mockedUserLogin);
@@ -79,7 +137,7 @@ describe("POST/users", () => {
     expect(response.status).toBe(403);
   });
 
-  test("DELETE /users/:id - should not be able to delete user without authentication", async () => {
+  test("DELETE /users/:id - Should not be possible to delete a user without admin permission", async () => {
     const adminLoginResp = await request(app)
       .post("/login")
       .send(mockedAdminLogin);
