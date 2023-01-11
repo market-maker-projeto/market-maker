204741c549887d51282d118721362388cab5e47c
JonathanVCB <viniciusjonathan@hotmail.com>
2023-01-11 10:21:16 -0300

    feat: add router patch test

--- src/tests/tables.test.ts
+++ src/tests/tables.test.ts
@@ -3,7 +3,11 @@ import AppDataSource from "../data-source";
 import request from "supertest";
 import app from "../app";
 import { createTableValid } from "./mocks/tables.mock";
-import { mockedAdmin } from "./mocks/users.mock";
+import {
+  mockedAdmin,
+  mockedAdminLogin,
+  mockedUserLogin,
+} from "./mocks/users.mock";
 
 describe("POST /tables", () => {
   let connection: DataSource;
@@ -114,5 +118,78 @@ describe("POST /tables", () => {
     expect(response.status).toBe(403);
   });
 
+  test("PATCH /tables/:id - should not be able to update tables without adm permission", async () => {
+    const newValues = { seats: 10 };
 
+    const userLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+    const admingLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const userToken = `Bearer ${userLoginResponse.body.token}`;
+    const adminToken = `Bearer ${admingLoginResponse.body.token}`;
+
+    const tableTobeUpdateRequest = await request(app)
+      .get(baseUrl)
+      .set("Authorization", adminToken);
+    const tableTobeUpdateId = tableTobeUpdateRequest.body[1].id;
+
+    const response = await request(app)
+      .patch(`/tables/${tableTobeUpdateId}`)
+      .set("Authorization", userToken)
+      .send(newValues);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
+
+  test("PATCH /tables/:id -  should be able to update table", async () => {
+    const newValues = { seats: 10, isActive: true };
+
+    const admingLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const token = `Bearer ${admingLoginResponse.body.token}`;
+
+    const tableTobeUpdateRequest = await request(app)
+      .get(baseUrl)
+      .set("Authorization", token);
+    const tableTobeUpdateId = tableTobeUpdateRequest.body[0].id;
+
+    const response = await request(app)
+      .patch(`/tables/${tableTobeUpdateId}`)
+      .set("Authorization", token)
+      .send(newValues);
+
+    const userUpdated = await request(app)
+      .get("/tables")
+      .set("Authorization", token);
+
+    expect(response.status).toBe(200);
+    expect(userUpdated.body[0].seats).toEqual(10);
+    expect(userUpdated.body[0].isActive).toEqual(true);
+  });
+
+  test("PATCH /tables/:id - should not be able to update table with invalid id", async () => {
+    const newValues = { seats: 10 };
+
+    const admingLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const token = `Bearer ${admingLoginResponse.body.token}`;
+
+    const tableTobeUpdateRequest = await request(app)
+      .get("/users")
+      .set("Authorization", token);
+    const tableTobeUpdateId = tableTobeUpdateRequest.body[0].id;
+
+    const response = await request(app)
+      .patch(`/users/13970660-5dbe-423a-9a9d-5c23b37943cf`)
+      .set("Authorization", token)
+      .send(newValues);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(404);
+  });
 });
