ef6e2a3127f1f53d4f42344e0e2e30681daa2808
jessicacolombo <jessicacolombo384@gmail.com>
2023-01-12 14:23:18 -0300

    create session service

--- src/services/session/createSession.service.ts
+++ src/services/session/createSession.service.ts
@@ -1,1 +1,35 @@
-export const createSessionService = async () => {};
+import { User } from "./../../entities/user.entity";
+import jwt from "jsonwebtoken";
+import { AppError } from "./../../errors/AppError";
+import AppDataSource from "../../data-source";
+import { IUserLogin } from "./../../interfaces/users.interfaces";
+import { compare } from "bcryptjs";
+
+export const createSessionService = async (userData: IUserLogin) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const user = await userRepository.findOneBy({
+    username: userData.username,
+  });
+
+  if (!user) {
+    throw new AppError("Wrong email/password.", 403);
+  }
+
+  const matchPassword = await compare(userData.password, user.password);
+
+  if (!matchPassword) {
+    throw new AppError("Wrong email/password.", 403);
+  }
+
+  const token = jwt.sign(
+    { username: userData.username },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "24h",
+      subject: user.id,
+    }
+  );
+
+  return token;
+};
