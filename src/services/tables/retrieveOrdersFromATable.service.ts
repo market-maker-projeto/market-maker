c4e9f5155a6d98ebf697ea208169eedc12f1a0a8
JonathanVCB <viniciusjonathan@hotmail.com>
2023-01-12 12:38:10 -0300

    feat: add get orders from table service

--- src/services/tables/retrieveOrdersFromATable.service.ts
+++ src/services/tables/retrieveOrdersFromATable.service.ts
@@ -1,1 +1,26 @@
-export const retrieveOrdersFromTableService = async () => {};
+import { AppError } from "./../../errors/AppError";
+import { Table } from "./../../entities/table.entity";
+import AppDataSource from "../../data-source";
+
+export const retrieveOrdersFromTableService = async (tableId) => {
+  const tableRepository = AppDataSource.getRepository(Table);
+
+  const tableExist = await tableRepository.findOneBy({
+    id: tableId,
+  });
+
+  if (!tableExist) {
+    throw new AppError("table not exist", 404);
+  }
+
+  const orders = await tableRepository.findOne({
+    where: {
+      id: tableId,
+    },
+    relations: {
+      orders: true,
+    },
+  });
+
+  return orders;
+};
