162552207e458b4ef050f9422b31140a6289e692
JonathanVCB <viniciusjonathan@hotmail.com>
2023-01-12 12:01:22 -0300

    feat: add get all tables service

--- src/services/tables/retrieveTables.service.ts
+++ src/services/tables/retrieveTables.service.ts
@@ -1,1 +1,10 @@
-export const retrieveTablesService = async () => {};
+import { Table } from "./../../entities/table.entity";
+import AppDataSource from "../../data-source";
+
+export const retrieveTablesService = async (): Promise<Table[]> => {
+  const tableRepository = AppDataSource.getRepository(Table);
+
+  const allTables = await tableRepository.find();
+
+  return allTables;
+};
