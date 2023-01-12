847ecbefbe94bb53303c4c8bd39282a5a28f5673
JonathanVCB <viniciusjonathan@hotmail.com>
2023-01-12 12:26:17 -0300

    feat: add update table service

--- src/services/tables/updateTable.service.ts
+++ src/services/tables/updateTable.service.ts
@@ -1,1 +1,25 @@
-export const updateTableService = async () => {};
+import { ITable } from "./../../interfaces/tables.interface";
+import { AppError } from "./../../errors/AppError";
+import { Table } from "./../../entities/table.entity";
+import AppDataSource from "../../data-source";
+
+export const updateTableService = async (data: ITable, tableId) => {
+  const tableRepository = AppDataSource.getRepository(Table);
+
+  const findTable = await tableRepository.findOneBy({
+    id: tableId,
+  });
+
+  if (!findTable) {
+    throw new AppError("table not exist", 404);
+  }
+
+  const updateTable = tableRepository.create({
+    ...findTable,
+    ...data,
+  });
+
+  await tableRepository.save(updateTable);
+
+  return updateTable;
+};
