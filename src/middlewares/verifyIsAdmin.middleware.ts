960bd85f880c0b07c9b7902e8d74e1c347fc6e7e
Guilherme Schulz <gui.schulz@gmail.com>
2023-01-12 09:41:36 -0300

    feat: added verify admin middleware

---
+++ src/middlewares/verifyIsAdmin.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+import 'dotenv/config'
+import { AppError } from './../errors/AppError';
+
+export const verifyAdminMiddleware =  async(req: Request, res: Response, next: NextFunction) => {
+    let foundUser = req.user
+
+    if(foundUser.isAdmin === false){
+         throw new AppError("User is not an administrator", 403)
+    }
+    return next()
+}
