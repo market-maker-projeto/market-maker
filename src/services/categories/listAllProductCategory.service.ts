d2b6fe991b1560b56c9416805ce18a093e871074
Henrique Jesus <henrique.jesus128@gmail.com>
2023-01-17 09:35:59 -0300

    fix: category list all product response

--- src/services/categories/listAllProductCategory.service.ts
+++ src/services/categories/listAllProductCategory.service.ts
@@ -1,17 +1,27 @@
 import AppDataSource from "../../data-source";
 import { Category } from "../../entities/category.entity";
 import { Product } from "../../entities/product.entity";
-import { IProductResponse } from "../../interfaces/products.interface";
+import { ICategoryProdResponse } from "../../interfaces/products.interface";
 
 export const listAllProductCategoryService = async (
   idCategory: string
-): Promise<IProductResponse[]> => {
+): Promise<ICategoryProdResponse[]> => {
   const productRepository = await AppDataSource.getRepository(Product);
 
   const listAllProduct = await productRepository.find({
     where: {
       category: { id: idCategory },
-    }
+    },
   });
-  return listAllProduct;
+
+  const listAllProductResponse = listAllProduct.map((element) => {
+    delete element.createdAt;
+    delete element.deletedAt;
+    delete element.updatedAt;
+    delete element.category.createdAt;
+    delete element.category.updatedAt;
+    return element
+  });
+
+  return listAllProductResponse;
 };
