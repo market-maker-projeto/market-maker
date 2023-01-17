ac699f60400161883c4ae6589e846af500d1f86a
JonathanVCB <viniciusjonathan@hotmail.com>
2023-01-17 10:59:42 -0300

    fix: remove properties on retrieveOrdersFromTableService and add schema on tables schemas

--- src/schemas/tables.schemas.ts
+++ src/schemas/tables.schemas.ts
@@ -1,4 +1,4 @@
-import { ItableRequest } from "./../interfaces/tables.interface";
+import { ItableRequest, ITableToOrder } from "./../interfaces/tables.interface";
 import * as yup from "yup";
 
 export const tableSerializer: yup.SchemaOf<ItableRequest> = yup.object().shape({
@@ -6,3 +6,48 @@ export const tableSerializer: yup.SchemaOf<ItableRequest> = yup.object().shape({
   isActive: yup.boolean().required(),
   table_number: yup.number().required(),
 });
+
+export const retrieveTableAndOrdersSerializer: yup.SchemaOf<any> = yup
+  .object()
+  .shape({
+    id: yup.string().required(),
+    seats: yup.number().required(),
+    isActive: yup.boolean().required(),
+    table_number: yup.number().required(),
+    orders: yup
+      .array(
+        yup
+          .object({
+            id: yup.string().required(),
+            client_name: yup.string().required(),
+            user: yup
+              .object({
+                id: yup.string().required(),
+                username: yup.string().required(),
+                isAdm: yup.boolean().required(),
+              })
+              .required(),
+            products: yup
+              .object({
+                id: yup.string().required(),
+                product: yup
+                  .object({
+                    id: yup.string().required(),
+                    name: yup.string().required(),
+                    price: yup.string().required(),
+                    in_stock: yup.boolean().required(),
+                    category: yup
+                      .object({
+                        id: yup.string().required(),
+                        name: yup.string().required(),
+                      })
+                      .required(),
+                  })
+                  .required(),
+              })
+              .required(),
+          })
+          .required()
+      )
+      .required(),
+  });
